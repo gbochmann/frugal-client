49644932b8aef869b39ded24dc109d319b77ec67
gbochmann <gbochmann@salesforce.com>
2021-12-28 07:42:38 +0100

    feat(tests): fix events tests

--- test/client/events_test.cljs
+++ test/client/events_test.cljs
@@ -8,6 +8,8 @@
 
 (def category-sums {["04" "2020"] {"Einkaufen" 896} ["04" "2021"] {"Einkaufen" 1198} ["12" "2021"] {"Restaurants" 3324 "Einkaufen" 500}})
 
+(def short-string "Buchung;Valuta;Auftraggeber/Empfänger;Buchungstext;Kategorie;Verwendungszweck;Saldo;Währung;Betrag;Währung\n07.04.2021;07.04.2021;VISA EDEKA AKTIV-MARKT;Lastschrift;Lebensmittel und Haushalt;NR XXXX 7033 JENA KAUFUMSATZ 03.04 195518 ARN74627641095000141960901 Apple Pay;687,44;EUR;-6,98;EUR\n07.04.2021;07.04.2021;Salesforce Germany GmbH;Lastschrift;Gehalt;Gehalt April 2020;6100;EUR;5325,00;EUR\n")
+
 (deftest test-transactions->category-freq
   (is (=
        (events/transactions->category-sum
@@ -60,31 +62,30 @@
   (let [counter (atom 1)] 
     (fn [] (str "UUID-" (swap! counter inc)))))
 
-;; (defn reset-counter [] (swap! counter (constantly 1)))
-
 (deftest test-add-fidor-transactions
   (is (=
        (with-redefs [random-uuid (make-uuid-mock)]
          (events/add-fidor-transactions
-          [{:transactions {"UUID-0" {}
-                           "UUID-1" {}}}
-           [::events/add-fidor-transactions
-            (event-result "Datum;Beschreibung;Beschreibung2;Wert\n02.11.2021;Irgendwas ausgegeben;;-5.99\n13.01.2022;Noch was ausgegeben;Für Synths;-689.99\n")]]))
+          {:transactions {"UUID-0" {} "UUID-1" {}}}
+          [::events/add-fidor-transactions
+           (event-result "Datum;Beschreibung;Beschreibung2;Wert\n02.11.2021;Irgendwas ausgegeben;;-5.99\n13.01.2022;Noch was ausgegeben;Für Synths;-689.99\n")]))
 
        {:transactions {"UUID-0" {}
                        "UUID-1" {}
-                       "UUID-2" {:date "02.11.2021" :note "Irgendwas ausgegeben" :expense 599 :income 0 :selected false :category nil :uuid "UUID-2"}
-                       "UUID-3" {:date "13.01.2022" :note "Noch was ausgegeben Für Synths" :expense 68999 :income 0 :selected false :category nil :uuid "UUID-3"}}})))
+                       "UUID-2" {:date [2 11 2021] :note "Irgendwas ausgegeben" :expense 599 :income 0 :selected false :category nil :uuid "UUID-2"}
+                       "UUID-3" {:date [13 1 2022] :note "Noch was ausgegeben Für Synths" :expense 68999 :income 0 :selected false :category nil :uuid "UUID-3"}}
+        :visible-transactions ["UUID-3" "UUID-2"]})))
 
 (deftest test-add-ing-transactions
   (is (=
        (with-redefs [random-uuid (make-uuid-mock)]
          (events/add-ing-transactions
-          [{:transactions {"UUID-0" {} "UUID-1" {}}}
-           [::events/add-ing-transactions
-            (event-result "Buchung;Valuta;Auftraggeber/Empfänger;Buchungstext;Kategorie;Verwendungszweck;Saldo;Währung;Betrag;Währung\n07.04.2021;07.04.2021;SCHOKOLADENHUNGER;SOFORT GEKAUFT;SUESSKRAM;NAHRUNGSAUFNAHME;687,44;EUR;-6,98;EUR\n13.01.2022;13.01.2022;Salesforce Germany GmbH;Lastschrift;Gehalt;Gehalt April 2020;6100;EUR;100000,00;EUR\n")]]))
+          {:transactions {"UUID-0" {} "UUID-1" {}}}
+          [::events/add-ing-transactions
+           (event-result "Buchung;Valuta;Auftraggeber/Empfänger;Buchungstext;Kategorie;Verwendungszweck;Saldo;Währung;Betrag;Währung\n07.04.2021;07.04.2021;SCHOKOLADENHUNGER;SOFORT GEKAUFT;SUESSKRAM;NAHRUNGSAUFNAHME;687,44;EUR;-6,98;EUR\n13.01.2022;13.01.2022;Salesforce Germany GmbH;Lastschrift;Gehalt;Gehalt April 2020;6100;EUR;100000,00;EUR\n")]))
 
        {:transactions {"UUID-0" {}
                        "UUID-1" {}
-                       "UUID-2" {:date "07.04.2021" :note "SOFORT GEKAUFT SCHOKOLADENHUNGER NAHRUNGSAUFNAHME" :expense 698 :income 0 :selected false :category nil :uuid "UUID-2"}
-                       "UUID-3" {:date "13.01.2022" :note "Lastschrift Salesforce Germany GmbH Gehalt April 2020" :expense 0 :income 10000000 :selected false :category nil :uuid "UUID-3"}}})))
+                       "UUID-2" {:date [7 4 2021] :note "SOFORT GEKAUFT SCHOKOLADENHUNGER NAHRUNGSAUFNAHME" :expense 698 :income 0 :selected false :category nil :uuid "UUID-2"}
+                       "UUID-3" {:date [13 1 2022] :note "Lastschrift Salesforce Germany GmbH Gehalt April 2020" :expense 0 :income 10000000 :selected false :category nil :uuid "UUID-3"}}
+        :visible-transactions ["UUID-3" "UUID-2"]})))
